224d67cbcbcaf3c768e0c1fa9908ac3d0f1f6994
felixesteban100 <felixesteban100@gmail.com>
2023-10-16 14:53:29 -0400

    better html in /ui route

--- app.ts
+++ app.ts
@@ -1,79 +1,96 @@
-require('dotenv').config()
-require('express-async-errors')
-import { Request, Response, NextFunction } from 'express';
-const serverless = require('serverless-http')
+require("dotenv").config();
+require("express-async-errors");
+import { Request, Response, NextFunction } from "express";
+const serverless = require("serverless-http");
 
 // extra security package
-const helmet = require('helmet')
-const cors = require('cors')
-const xss = require('xss-clean')
-const rateLimiter = require('express-rate-limit')
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
 
 // importe the framework of nodejs (express)
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-// routers 
-const authRouter = require('./routes/auth')
+// routers
+const authRouter = require("./routes/auth");
 
-// error handler 
-const notFoundMiddleware = require('./middleware/not-found')
-const errorMiddleware = require('./middleware/error-handler')
+// error handler
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
 
 // this is for deploying purposes
-app.set('trust proxy', 1)
+app.set("trust proxy", 1);
 
 // Allow CORS requests from any origin with any method and headers
 app.use((_: Request, res: Response, next: NextFunction) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
 
-// using extra packages 
-app.use(rateLimiter({
+// using extra packages
+app.use(
+  rateLimiter({
     window: 15 * 16 * 1000, // 15 minutes
-    max: 100 // milit each IP to 100 request per windowMs
-}))
-app.use(express.json())
-app.use(helmet())
-app.use(cors())
-app.use(xss())
+    max: 100, // milit each IP to 100 request per windowMs
+  })
+);
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      imgSrc: ["'self'", "https://images.crunchbase.com"],
+      // Add other directives as needed
+    },
+  })
+);
 
 // UI
-app.get('/ui', (req: Request, res: Response) => {
-    res.sendFile('index.html', { root: 'src' })
-})
+app.use(express.static("src"));
+
+app.use("/ui", (req: Request, res: Response) => {
+  res.sendFile("index.html", { root: "src" });
+});
 
 // routes
-app.use('/', authRouter)
+app.use("/", authRouter);
 
 //using own middleware
-app.use(notFoundMiddleware)
-app.use(errorMiddleware)
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
 
-process.on('uncaughtException', (error) => {
-    console.error('Uncaught Exception:', error);
-    // Optionally, you can perform any necessary cleanup here
-    process.exit(1); // Exit the process with an error code (1)
+process.on("uncaughtException", (error) => {
+  console.error("Uncaught Exception:", error);
+  // Optionally, you can perform any necessary cleanup here
+  process.exit(1); // Exit the process with an error code (1)
 });
 
 const port = process.env.PORT?.toString() || "5000";
 
 async function start() {
-    try {
-        app.listen(port, () => console.log(`http://localhost:${port}`))
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    app.listen(port, () => console.log(`http://localhost:${port}`));
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 // FIX THE DEPLOYMENT ONRENDER
 
-start()
+start();
 
 /* npm i bcryptjs cors dotenv express express-async-errors express-rate-limit helmet http-status-codes joi jsonwebtoken mongoose rate-limiter swagger-ui-express 
 xss-clean yamljs */
 
-module.exports.handler = serverless(app)
+module.exports.handler = serverless(app);
